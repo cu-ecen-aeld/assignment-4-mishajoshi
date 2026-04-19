9df65bc9db5ef1fafedaaa40ffb37452ddec8c9d
mjoshi <mjoshi@Ubuntu.myguest.virtualbox.org>
2026-04-18 22:18:41 -0400

    assignment 7 complete

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# ldd (Linux Device Drivers modules)
+#
+################################################################################
+
+LDD_VERSION = 0dfb311de3fbc6eff488724de2ee72147066d9d3
+LDD_SITE = https://github.com/cu-ecen-aeld/assignment-7-mishajoshi.git
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+
+LDD_MODULE_SUBDIRS = misc-modules scull
+
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin/
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
